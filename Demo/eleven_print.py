298f1ea7a87f465cdf0e9c03af94c7705dba86cd
madhuchandrak <madhuchandr@gmail.com>
2019-09-29 22:14:59 +0200

    Adding eleven_print.py test script

---
+++ Demo/eleven_print.py
@@ -0,0 +1,9 @@
+print("Success can only be delayed,not denied")
+
+print('Software Quality Assurance Engineer, Software QA Engineer, QA Engineer, Quality Analyst')
+
+print("Senior Test Engineer, Senior QA Engineer, Test Automation Engineer, Senior Automation Engineer")
+
+print("Software Automation Test Engineer, Automation Lead, QA Lead, Test Lead")
+
+print("Volkswagen Beetle, 1960 to 2003")
